e2bc5af5a8a43533b89b1e3f69b7440c1f9f41fa
jpez2051 <166471122+jpez2051@users.noreply.github.com>
2026-08-13 09:16:33 -1000

    feat: mount settings center

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import DataSafety from './DataSafety'
+import SettingsCenter from './SettingsCenter'
 import './styles.css'
 import './v041.css'
 import './v043.css'
 import './v050.css'
 import './v051.css'
+import './v052.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <DataSafety />
+    <SettingsCenter />
   </React.StrictMode>,
 )
